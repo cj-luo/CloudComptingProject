9e800f3c658cab77375360456834dfb249e60a79
caijuanluo <cj_luo@outlook.com>
2021-10-05 02:31:10 -0700

    fix login link

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -10,15 +10,12 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>Welcome to the Note</p>
-                <a
-                    className="App-link"
-                    href = "https://cj-luo-midterm.auth.us-east-1.amazoncognito.com/login?client_id=38pssvqqhceonfr46fcrjur490&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://aws.cj-luo-midterm.link/"
-                    target="_blank"
-                    rel="noopener noreferrer"
+                <button
+                    type="button" class="btn btn-primary"
+                    onClick = {() => window.location.href = "https://cj-luo-midterm.auth.us-east-1.amazoncognito.com/login?client_id=38pssvqqhceonfr46fcrjur490&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://aws.cj-luo-midterm.link/"}
                 >
                     Click Here to Login
-                </a>
-                
+                </button>
             </header>
         </div>
     );
